e190bd5f96e8d104a9bd6d35d7dbd89a8faf1f2e
biswajitbg <babughosh7584@gmail.com>
2022-06-16 21:02:50 +0530

    auth assignment1

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://biswajit10:RqGx288M-J3sVWE@cluster0.vqgtj.mongodb.net/biswajit10?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
